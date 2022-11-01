f210d09fab888db8f3c8279d6f8cca1313ba19df
gustavocadev <churaalaygustavo11@gmail.com>
2022-11-01 11:55:42 -0500

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import './style.css';
+
+const form = document.querySelector<HTMLFormElement>('form');
+
+type Data = {
+  name: string;
+  email: string;
+  telephone: string;
+  message: string;
+};
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.currentTarget);
+  const data = Object.fromEntries(formData) as Data;
+
+  const { email, message, name, telephone } = data;
+  if (!email || !message || !name || !telephone) {
+    alert('Por favor, complete todos los campos');
+    return;
+  }
+
+  alert(
+    `${data.name}, Gracias por contactarte, te responderemos a la brevedad`
+  );
+  form.reset();
+});
